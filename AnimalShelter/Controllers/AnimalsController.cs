6c4d14c91fa950f00eb7a6e266e21b537b6b5bea
Beverly Potts <pottsbeverly@gmail.com>
2020-08-21 13:17:46 -0400

    Add PUT and DELETE routes for animals

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Controllers
 {
@@ -33,6 +34,22 @@ namespace AnimalShelter.Controllers
     public ActionResult<Animal> Get(int id)
     {
       return _db.Animals.FirstOrDefault(entry => entry.AnimalId ==id);
+    }
+
+    [HttpPut("{id}")] //PUT api/animals/5
+    public void Put(int id, [FromBody] Animal animal)
+    {
+      animal.AnimalId = id;
+      _db.Entry(animal).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    [HttpDelete("{id}")]//DELETE api/animals/5
+    public void Delete(int id)
+    {
+      var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId ==id);
+      _db.Animals.Remove(animalToDelete);
+      _db.SaveChanges();
     }
   }
 }

97ffeaa6d31650417de1a40fdd835934f289e725
Beverly Potts <pottsbeverly@gmail.com>
2020-08-21 14:45:49 -0400

    Add validation for Animal instance creation

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(30, ErrorMessage = "Please enter a name under 30 characters.")]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
+    [Required]
     public string Breed { get; set; }
+    [Required]
+    [Range(0, 40, ErrorMessage = "Please enter an age between 0 and 40.")]
     public int Age { get; set; }
+    [Required]
     public string Gender { get; set; }
   }
 }
